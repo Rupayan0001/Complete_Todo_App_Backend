10b94deec3f8f2a0556e0b9f3a894479c6fe725e
Rupayan <154427141+Rupayan0001@users.noreply.github.com>
2024-10-09 16:41:24 +0530

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const JWT_SECRET = process.env.JWT_SECRET;
+const jwt = require("jsonwebtoken");
+function auth(req, res, next) {
+    try {
+        const token = req.headers.token;
+        const decode = jwt.verify(token, JWT_SECRET);
+        if (decode) {
+            req.userId = decode.id;
+            console.log(req.userId)
+            console.log("Auth success")
+            next();
+        } else {
+            res.status(403).json({
+                message: "You are not logged in"
+            })
+        }
+
+    } catch (error) {
+        res.status(403).json({
+            message: "You are not logged in"
+        })
+    }
+}
+
+module.exports = {
+    auth,
+    JWT_SECRET,
+}
